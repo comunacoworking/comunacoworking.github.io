682fba363444b8dc9a36497ac861f2480109b53c
Indexo-Git <contacto@indexo.mx>
2017-04-26 10:18:42 -0500

    Cambios indexo

--- js/main.js
+++ js/main.js
@@ -73,12 +73,11 @@ $(function() {
 
 
 /*Indexo js*/
-
 (function(){
   // setup your carousels as you normally would using JS
   // or via data attributes according to the documentation
   // http://getbootstrap.com/javascript/#carousel
-  $('#carousel123').carousel({ interval: 3000 });
+  $('#carousel123').carousel({ interval: 2000 });
 }());
 
 (function(){
